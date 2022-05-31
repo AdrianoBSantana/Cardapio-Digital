9cbf90765873973728ede555889253331b2b9b86
Adriano <adrianobs44@gmail.com>
2022-05-30 23:12:37 -0300

    Projeto 2

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,29 @@
+import "./Home.css"
+import PaletaLista from "components/PaletaLista/PaletaLista"
+import Navbar from "components/Navbar/Navbar"
+import sacola from "assets/icons/sacola.svg"
+import logo from "assets/logo.svg"
+
+function Home(){
+
+    
+
+    let isTrue = true
+    
+    return (
+        <div className="Home" 
+        
+            style={{margin: "15px", padding: '15px'}}> 
+            {}
+            <Navbar 
+                logo={logo}
+                sacola={sacola}
+            />
+            <div className="Home__container">
+                <PaletaLista />
+            </div>    
+        </div>
+        )
+}
+
+export default Home
